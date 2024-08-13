167214809b71f37b5e4e9f4b623c49d641bc9744
LSDog <LSDog@foxmail.com>
2024-08-13 20:35:34 +0800

    Add topnav

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./App.css"
 
-function App() {
-  const [count, setCount] = useState(0)
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    return (
+        <>
+            <TopNav />
+        </>
+    )
 }
 
-export default App
+/**
+ * 顶部导航栏
+ */
+function TopNav() {
+    return (
+        <div className="topnav">
+
+            {/* 左侧选项 */}
+            <img src="https://s3.bmp.ovh/imgs/2024/08/09/81b4ff5e5f614e0b.jpg" className="left icon"></img>
+            <a href="#" className="left title">西苑萌战官方网站！</a>
+
+            {/* 右侧选项 */}
+            {/* 这里的左右顺序和上下顺序是反的 */}
+            <a href="#" className="right option">登录/注册</a>
+            <a herf="#" className="right option">我要捐赠</a>
+            <a herf="#" className="right option">开发者们</a>
+            <a herf="#" className="right option">关于西苑</a>
+            <a herf="#" className="right option">关于萌战</a>
+        
+        </div>
+    );
+}
